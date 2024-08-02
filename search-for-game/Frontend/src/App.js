fa95b34d027369f039af49c8e92c609fbafddb03
Devin Zhang <dzhang333@wisc.edu>
2024-08-02 03:51:35 -0500

    added search and favorites page

--- search-for-game/Frontend/src/App.js
+++ search-for-game/Frontend/src/App.js
@@ -1,18 +1,20 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
 import GameHub from './Pages/GameHub/GameHub';
+import Favorites from './Pages/Favorites/Favorites'
+import Search from './Pages/Search/Search';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   useEffect(() => {
     fetch('http://localhost:8801/user')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
   }, [])
   return (
     <Router>
@@ -20,9 +22,11 @@ function App() {
         <header className="App-header">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/gamehub" element={<GameHub />} /> 
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/gamehub" element={<GameHub />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </header>
       </div>
--- search-for-game/Frontend/src/Pages/GameHub/GameHub.jsx
+++ search-for-game/Frontend/src/Pages/GameHub/GameHub.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Col, Row } from 'react-bootstrap';
 import './GameHub.css';
+import GameCard from '../../components/GameCard';
 
 function GameHub() {
   const navigate = useNavigate();
   const [games, setGames] = useState([]);
+
+  // navigations
+  const goToHome = () => {
+    sessionStorage.removeItem('username')
+    navigate('/')
+  }
+  const goToFavorites = () => navigate('/favorites')
+  const goToSearch = () => navigate('/search')
 
   useEffect(() => {
     // Fetch the 10 most popular games
@@ -16,34 +24,35 @@ function GameHub() {
       .catch((error) => console.error('Error fetching games:', error));
   }, []);
 
-  // Function to handle navigation to home
-  const goToHome = () => {
-    navigate('/'); // Navigate to Home page
-  };
-
   return (
     <div className="gamehub-container">
-      <Button onClick={goToHome} className="home-button" variant="secondary">
-        Back to Home
-      </Button>
-      <h2>Welcome to the GameHub!</h2>
-      <p>Here you can access all your favorite games.</p>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+        <Button onClick={goToHome} className="home-button" variant="secondary">
+          Back to Home
+        </Button>
+        <Button onClick={goToFavorites} className="favorite-button" variant="secondary">
+          My Favorites
+        </Button>
+      </div>
+      <Row className="justify-content-center">
+        <h2>Welcome to the GameHub!</h2>
+        <Col xs="auto">
+          <p>Here you can access all your favorite games.</p>
+        </Col>
+        <Col xs="auto">
+          <Button onClick={goToSearch} className="search-button" variant="primary">
+            Search for Games!
+          </Button>
+        </Col>
+      </Row>
+      <h1>Top Tens</h1>
       <div className="games-grid">
         {games.map((game) => (
-          <Card key={game.Name} style={{ width: '18rem', margin: '1rem' }}>
-            <Card.Img variant="top" src={game.Image} alt={game.Name} />
-            <Card.Body>
-              <Card.Title>{game.Name}</Card.Title>
-              <Card.Text>
-                Price: ${game.Price}
-              </Card.Text>
-              <Button variant="primary">Expand</Button>
-            </Card.Body>
-          </Card>
+          <GameCard key={game.AppId} game={game} />
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 export default GameHub;
